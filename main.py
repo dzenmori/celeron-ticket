a8d1632cfd0b54ab97933c68e4f2e3b9d5e8c8e8
Rama44444 <ramadian036@gmail.com>
2025-10-04 15:18:19 +0700

    tambahkan fungsi pemesanan

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-for i in range(10):
-    print(i)
+tiket = {
+    "1" : 200000,
+    "2" : 150000,
+    "3" : 180000,
+    "4" : 200000,
+    "5" : 100000
+}
+
+# tiket = [20000, 150000, 180000, 200000, 100000]
+
+data = []
+
+def tambah():
+    nama = input('Masukan nama pelanggan : ')
+    print("""
+    Kota Tujuan tersedia :
+    1.Jakarta
+    2.Bandar Lampung
+    3.Bandung
+    4.Yogyakarta
+    5.Palembang
+""")
+
+    Tujuan = input('pilih no kota tujuan :')
+
+    if Tujuan in tiket:
+        jumlah = int(input('Masukan Jumlah tiket :'))
+        total = tiket[Tujuan] * jumlah
+        print(f"Total harga tiket : {total}")
+
+    else:
+        print('Kota tujuan tidak tersedia')
+tambah()
+
